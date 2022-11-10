624c30cc33b7d3c17ba5172edb8e08aeef69549f
indigo030 <90837372+indigo030@users.noreply.github.com>
2022-11-10 21:11:56 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'DuGuorui\'s Blog',
-  author: 'Du Guorui',
+  title: 'Guorui's Blog',
+  author: 'Guorui Du',
   email: 'indigo20030103@gmail.com',
   link: 'https://nobelium.vercel.app',
   description: 'We\'re all gonna die, lets act accordingly.',
